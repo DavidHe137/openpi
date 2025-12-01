a0711159377c13f6b4b04d06ce10444990d37085
David He <davidhe137@gmail.com>
2025-11-30 20:18:25 -0500

    Delete abstract methods so libero example works.

--- packages/openpi-client/src/openpi_client/base_policy.py
+++ packages/openpi-client/src/openpi_client/base_policy.py
@@ -7,14 +7,6 @@ class BasePolicy(abc.ABC):
     def infer(self, obs: Dict) -> Dict:
         """Infer actions from observations."""
 
-    @abc.abstractmethod
-    def infer_batch(self, obs_batch: list[Dict]) -> list[Dict]:
-        """Infer actions from a batch of observations."""
-
     def reset(self) -> None:
         """Reset the policy to its initial state."""
         pass
-
-    @abc.abstractmethod
-    def make_example(self) -> Dict:
-        """Make an example observation for the policy."""
